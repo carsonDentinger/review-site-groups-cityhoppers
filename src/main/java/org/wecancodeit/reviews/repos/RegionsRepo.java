dc816baf4c4f0ab1177a92603968d010cbda980c
jgard2019 <jwgardner22@gmail.com>
2021-10-20 13:37:53 -0400

    more working formsgit add .

--- src/main/java/org/wecancodeit/reviews/repos/RegionsRepo.java
+++ src/main/java/org/wecancodeit/reviews/repos/RegionsRepo.java
@@ -3,5 +3,8 @@ package org.wecancodeit.reviews.repos;
 import org.springframework.data.repository.CrudRepository;
 import org.wecancodeit.reviews.model.Region;
 
+import java.util.Optional;
+
 public interface RegionsRepo extends CrudRepository<Region, Long> {
+    Optional<Region> findByRegionNameIgnoreCase(String regionName);
 }
